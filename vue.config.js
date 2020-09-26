4f4a4732830ca3bd7e9004ede4cb3baa51b4e264
csiew <clarence.siew@gmail.com>
2020-09-27 02:36:34 +1000

    Attempt to fix Vue Router issue with GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-crash-course/'
-    : '/'
+    baseUrl: '/vue-crash-course/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-crash-course/' : '/'
 }
