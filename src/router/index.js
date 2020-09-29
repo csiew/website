93bc1f32b7a99a6bda0c96e8ef9410e4ba9802e6
csiew <clarence.siew@gmail.com>
2020-09-29 17:39:22 +1000

    Revert to hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -36,9 +36,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  history: createWebHistory(process.env.BASE_URL),
+  mode: 'hash',
   routes
 });
 
