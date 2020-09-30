ed90ac71fa6eff4c92726b3f826bef6385559768
csiew <clarence.siew@gmail.com>
2020-09-30 21:25:45 +1000

    Refactored skills into a JSON file

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -5,8 +5,9 @@
     </div>
     <div class="card padding-m">
       <div class="margin-l-bottom">
-        <form>
+        <form class="hstack">
           <input class="width-full" type="text" placeholder="Filter..." @keyup="filter" v-model="filterTerm" />
+          <button v-if="this.filterTerm.length > 0" class="margin-s-left cursor-pointer" v-on:click="this.resetResults()">Clear</button>
         </form>
       </div>
       <span v-if="this.filterTerm.length !== 0">
@@ -24,7 +25,7 @@
         <div class="grid grid-gap-m grid-col-1 grid-col-auto-fit-640">
           <div class="grid-item list-freestyle">
             <ul>
-              <li v-for="item in this.programmingLangs" v-bind:key="item">{{ item }}</li>
+              <li v-for="item in this.skills['programmingLangs']" v-bind:key="item">{{ item }}</li>
             </ul>
           </div>
         </div>
@@ -34,13 +35,13 @@
           <div class="grid-item list-freestyle">
             <h5>Frontend</h5>
             <ul>
-              <li v-for="item in this.frameworks.frontend" v-bind:key="item">{{ item }}</li>
+              <li v-for="item in this.skills['frameworks']['frontend']" v-bind:key="item">{{ item }}</li>
             </ul>
           </div>
           <div class="grid-item list-freestyle">
             <h5>Backend</h5>
             <ul>
-              <li v-for="item in this.frameworks.backend" v-bind:key="item">{{ item }}</li>
+              <li v-for="item in this.skills['frameworks']['backend']" v-bind:key="item">{{ item }}</li>
             </ul>
           </div>
         </div>
@@ -50,19 +51,19 @@
           <div class="grid-item list-freestyle">
             <h5>Database</h5>
             <ul>
-              <li v-for="item in this.infrastructure.database" v-bind:key="item">{{ item }}</li>
+              <li v-for="item in this.skills['infrastructure']['database']" v-bind:key="item">{{ item }}</li>
             </ul>
           </div>
           <div class="grid-item list-freestyle">
             <h5>Cloud</h5>
             <ul>
-              <li v-for="item in this.infrastructure.cloud" v-bind:key="item">{{ item }}</li>
+              <li v-for="item in this.skills['infrastructure']['cloud']" v-bind:key="item">{{ item }}</li>
             </ul>
           </div>
           <div class="grid-item list-freestyle">
             <h5>Continuous Integration</h5>
             <ul>
-              <li v-for="item in this.infrastructure.ci" v-bind:key="item">{{ item }}</li>
+              <li v-for="item in this.skills['infrastructure']['ci']" v-bind:key="item">{{ item }}</li>
             </ul>
           </div>
         </div>
@@ -72,52 +73,20 @@
 </template>
 
 <script>
+import skillsJSON from '@/assets/skills.json'
+
 export default {
   name: 'Skills',
   data() {
     return {
-      programmingLangs: [
-        'JavaScript',
-        'Python',
-        'Swift',
-        'Java',
-        'Kotlin',
-        'HTML',
-        'CSS',
-        'Bash'
-      ],
-      frameworks: {
-        frontend: [
-          'React',
-          'SwiftUI'
-        ],
-        backend: [
-          'Node.js',
-          'Express',
-          'Spring'
-        ]
-      },
-      infrastructure: {
-        database: [
-          'PostgreSQL',
-          'MySQL',
-          'Firestore (NoSQL)'
-        ],
-        cloud: [
-          'EC2, CloudWatch (Amazon AWS)',
-          'Google Firebase'
-        ],
-        ci: [
-          'GitHub Pages'
-        ]
-      },
+      skills: skillsJSON,
       filterTerm: '',
       result: []
     }
   },
   methods: {
     filter: function () {
-      this.result = this.programmingLangs.concat(this.frameworks.frontend, this.frameworks.backend, this.infrastructure.database, this.infrastructure.cloud, this.infrastructure.ci).filter(item => item.toLowerCase().includes(this.filterTerm.toLowerCase()));
+      this.result = this.skills['programmingLangs'].concat(this.skills['frameworks']['frontend'], this.skills['frameworks']['backend'], this.skills['infrastructure']['database'], this.skills['infrastructure']['cloud'], this.skills['infrastructure']['ci']).filter(item => item.toLowerCase().includes(this.filterTerm.toLowerCase()));
     },
     resetResults: function () {
       this.result = [];
