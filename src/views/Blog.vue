af038ab923e8d6e0c34ae852ac677772dcc078bf
csiew <clarence.siew@gmail.com>
2021-02-05 15:03:42 +1100

    New blog post and reverse order blog post list

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -6,7 +6,7 @@
       </div>
       <div v-if="Object.values(this.posts).length > 0" class="card card-enter card-list">
         <ul>
-          <li v-for="post in this.posts" v-bind:key="post.id" class="width-full">
+          <li v-for="post in Object.values(this.posts).reverse()" v-bind:key="post.id" class="width-full">
             <router-link v-bind:to="{ name: 'Post', params: { id: post.id } }" class="width-full flex-inline flex-flow-column align-start justify-start padding-s padding-xs-top padding-xs-bottom">
               <h3 class="ambigious-link" style="line-height: 1.125;">{{ post.title }}</h3>
               <small class="timestamp text-color-secondary">{{ this.formatTimestamp(post.date) }}</small>
