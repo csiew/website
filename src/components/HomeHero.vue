4a57c46b5801569fff33aa8c9990c7335688f645
Clarence Siew <clarence.siew@gmail.com>
2021-05-11 23:56:20 +1000

    Restore chevron-style arrows

--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -21,8 +21,8 @@
                 <button v-for="(item, index) in projects" v-bind:key="item.name" class="tabbar-item" v-bind:class="currentIndex === index ? 'tabbar-item-current' : ''" v-on:click="setIndex(index)">{{ index + 1 }}</button>
               </div>
               <div class="tabbar width-auto margin-xs-left">
-                <button class="tabbar-item" v-on:click="prevIndex">&xlarr;</button>
-                <button class="tabbar-item" v-on:click="nextIndex">&xrarr;</button>
+                <button class="tabbar-item" v-on:click="prevIndex">&#10094;</button>
+                <button class="tabbar-item" v-on:click="nextIndex">&#10095;</button>
               </div>
             </div>
           </div>
@@ -30,7 +30,7 @@
             <img v-if="isVisible.projectScreenshot === true" v-bind:src="getImgUrl(projects[currentIndex].imageUrl)" v-bind:alt="projects[currentIndex].name" class="card card-img nodrag noselect" style="width: 100%; height: auto;" />
           </transition>
           <div class="hstack width-full align-center justify-center padding-m-top">
-            <router-link class="button padding-m-left padding-m-right" v-bind:to="{ name: 'Projects' }">See all projects &xrarr;</router-link>
+            <router-link class="button padding-m-left padding-m-right" v-bind:to="{ name: 'Projects' }">See all projects &#10095;</router-link>
           </div>
         </div>
       </div>
