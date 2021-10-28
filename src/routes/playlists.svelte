4ee5302d5eb9c4d5fda41291641c338bfc89b5d9
Clarence Siew <clarence.siew@gmail.com>
2021-10-28 21:11:47 +1100

    Responsive design tweaks

--- src/routes/playlists.svelte
+++ src/routes/playlists.svelte
@@ -1,7 +1,8 @@
 <script context="module" lang="ts">
-	import { dev } from '$app/env';
+	import { browser, dev } from '$app/env';
 	
 	export const hydrate = dev;
+	export const router = browser;
 	export const prerender = false;
 </script>
 
