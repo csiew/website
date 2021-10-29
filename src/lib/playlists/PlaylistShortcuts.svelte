33e29a6a0536cdff7df179a8d6523724d5f150c0
Clarence Siew <clarence.siew@gmail.com>
2021-10-29 19:24:46 +1100

    New layout and styling for page shortcuts toast window

--- src/lib/playlists/PlaylistShortcuts.svelte
+++ src/lib/playlists/PlaylistShortcuts.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-  import { fly } from 'svelte/transition';
+  import { fade } from 'svelte/transition';
 
   export let playlistYears: PlaylistYear[];
 
   let hide: boolean = true;
 </script>
 
-<div class="toast-window">
+<div class="toast-window" class:toast-window-open={!hide}>
   <div class="heading" on:click|preventDefault={() => hide = !hide}>
     <span class="title">
       Shortcuts
     </span>
   </div>
   {#if !hide}
-    <div class="body" in:fly={{ y: 500, duration: 200 }} out:fly={{ y: 500, duration: 200 }}>
+    <div class="body" in:fade={{ duration: 200 }} out:fade={{ duration: 200 }}>
       <ul>
         {#each playlistYears as year}
           <li><a href={`#${year.title}`}>{year.title}</a></li>
