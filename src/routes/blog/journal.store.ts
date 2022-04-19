d6d07566aad5b885a25574eea1863f0a78459877
Clarence Siew <clarence.siew@gmail.com>
2022-04-19 23:08:35 +1000

    [5.7.1] Put blog store initialisation in Writable Store declaration

--- src/routes/blog/journal.store.ts
+++ src/routes/blog/journal.store.ts
@@ -8,34 +8,20 @@ export type BlogPost = {
   content: string;
 };
 
-export const store: Writable<BlogPost[]> = writable([]);
+export const store: Writable<BlogPost[]> = writable(
+  rawPosts.map((post) => {
+    return {
+      ...post,
+      content: decodeURI(post.content)
+    } as BlogPost;
+  })
+);
 
 export function getPost(id: string): BlogPost | null {
   let result: BlogPost;
   store.update((posts: BlogPost[]) => {
-    if (posts.length === 0) {
-      posts = initialiseStore();
-    }
     result = posts.find((post) => post.id === id);
     return posts;
-  });
-  return result;
-}
-
-export function initialiseStore(): BlogPost[] {
-  let result: BlogPost[];
-  store.update((posts) => {
-    if (posts.length === 0) {
-      result = rawPosts.map((post) => {
-        return {
-          ...post,
-          content: decodeURI(post.content)
-        } as BlogPost;
-      });
-      return result;
-    } else {
-      return posts;
-    }
   });
   return result;
 }
