ba89f00efb0a617f8bc21856ee7e5ee86bc93027
Clarence Siew <clarence.siew@gmail.com>
2022-09-01 05:51:52 +1000

    Grid view for Now Watching page

--- components/app/NowWatchingShowCard/index.tsx
+++ components/app/NowWatchingShowCard/index.tsx
@@ -1,28 +1,17 @@
 import React from "react";
 import { Show } from "../../../lib/now-watching";
-import Button from "../../ui/Button";
-import Paper from "../../ui/Paper";
 
 const NowWatchingShowCard = ({ show, keyPrefix }: { show: Show, keyPrefix: string }) => {
   return (
-    <section id={`${keyPrefix}-${show.imdbId}`}>
-      <Paper className="nowWatchingShowCard">
-        <div className="poster">
-          <img src={show.metadata?.Poster} alt={show.name} />
-        </div>
-        <div className="metadata">
-          <div>
-            <h4>{show.name}</h4>
-            <sub>{show.metadata?.Genre}, {show.metadata?.Year}</sub>
-          </div>
-          <p>
-            {show.metadata?.Plot}
-          </p>
-          <Button url={`https://imdb.com/title/${show.imdbId}`} newTab={true}>
-            View in IMDB
-          </Button>
-        </div>
-      </Paper>
+    <section id={`${keyPrefix}-${show.imdbId}`} className="nowWatchingShowCard">
+      <div className="poster">
+        <img src={show.metadata?.Poster} alt={show.name} />
+      </div>
+      <div className="metadata">
+        <h4>{show.name}</h4>
+        <sub>{show.metadata?.Genre}</sub>
+        <sub>{show.metadata?.Year}</sub>
+      </div>
     </section>
   );
 };
