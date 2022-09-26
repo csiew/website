980a61ee7e20f5b88d001314572b3263aba06800
Clarence Siew <clarence.siew@gmail.com>
2022-09-27 00:28:11 +1000

    Fix timestamp utility

--- lib/timestamp.ts
+++ lib/timestamp.ts
@@ -1,35 +1,45 @@
 export const relativeTime = (timestamp: Date | string) => {
   const diffInSeconds = Math.floor((Date.now() - new Date(timestamp).getTime())/1000);
+  const rawDiffs = {
+    seconds: diffInSeconds,
+    minutes: diffInSeconds/60,
+    hours: diffInSeconds/60/60,
+    days: diffInSeconds/60/60/24,
+    weeks: diffInSeconds/60/60/24/7,
+    months: diffInSeconds/60/60/24/30,
+    years: diffInSeconds/60/60/24/365
+  };
+  const roundedDiffs = Object.entries(rawDiffs).reduce((diffs, [k, v]) => diffs.set(k, Math.floor(v)), new Map<string, number>());
   
   if (diffInSeconds < 60) {
     // Seconds ago
-    return `${diffInSeconds} second${diffInSeconds > 1 ? "s" : ""} ago`;
+    const seconds = roundedDiffs.get("seconds") ?? 0;
+    return `${seconds} second${seconds > 1 ? "s" : ""} ago`;
   } else if (diffInSeconds < 3600) {
     // Minutes ago
-    const diffInMinutes = Math.floor(diffInSeconds/60);
-    return `${diffInMinutes} minute${diffInMinutes > 1 ? "s" : ""} ago`;
+    const months = roundedDiffs.get("months") ?? 0;
+    return `${months} minute${months > 1 ? "s" : ""} ago`;
   } else if (diffInSeconds < 86400) {
     // Hours ago
-    const diffInHours = Math.floor(diffInSeconds/60/60);
-    return `${diffInHours} hour${diffInHours > 1 ? "s" : ""} ago`;
+    const hours = roundedDiffs.get("hours") ?? 0;
+    return `${hours} hour${hours > 1 ? "s" : ""} ago`;
   } else if (diffInSeconds < 604800) {
     // Days ago
-    const diffInDays = Math.floor(diffInSeconds/60/60/24);
-    return `${diffInDays} day${diffInDays > 1 ? "s" : ""} ago`;
+    const days = roundedDiffs.get("days") ?? 0;
+    return `${days} day${days > 1 ? "s" : ""} ago`;
   } else if (diffInSeconds < 2592000) {
     // Weeks ago
-    const diffInWeeks = Math.floor(diffInSeconds/60/60/24/7);
-    return `${diffInWeeks} week${diffInWeeks > 1 ? "s" : ""} ago`;
+    const weeks = roundedDiffs.get("weeks") ?? 0;
+    return `${weeks} week${weeks > 1 ? "s" : ""} ago`;
   } else if (diffInSeconds < 31536000) {
     // Months ago
-    const diffInMonths = Math.floor(diffInSeconds/60/60/24/30);
-    return `${diffInMonths} month${diffInMonths > 1 ? "s" : ""} ago`;
+    const months = roundedDiffs.get("months") ?? 0;
+    return `${months} month${months > 1 ? "s" : ""} ago`;
   } else {
     // Years ago
-    const rawDiffInYears = diffInSeconds/60/60/24/365;
-    const diffInYears = Math.floor(rawDiffInYears);
-    const remainder = rawDiffInYears - diffInYears;
-    const remainderInMonths = Math.floor(remainder * 12);
-    return `${diffInYears} year${diffInYears > 1 ? "s" : ""}, ${remainderInMonths} month${remainderInMonths > 1 ? "s" : ""} ago`;
+    const years = roundedDiffs.get("years") ?? 0;
+    const remainder = rawDiffs.years - Math.floor(rawDiffs.years);
+    const remainderInMonths = Math.round((remainder * 365) / 30);
+    return `${years} year${years > 1 ? "s" : ""}, ${remainderInMonths} month${remainderInMonths > 1 ? "s" : ""} ago`;
   }
 };
