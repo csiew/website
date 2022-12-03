0c9e66ebb60602171fe0c3fae4f4bf6af6143e98
Clarence <clarence.siew@gmail.com>
2022-12-04 01:33:59 +1100

    [6.4.1] New navigation menu (#59)
    * Transform BackToHome into new navbar
    
    * Migrate BackToHome to Navbar component
    
    * Bump version number and add to nav menu
    
    * Rename Navbar back to NavBar
    
    * Fix CSS import capitalisation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import "./app.css";
-import BackToHome from "../components/app/BackToHome";
+import NavBar from "../components/app/NavBar";
 import BackToTop from "../components/app/BackToTop";
 import config from "../config";
 
@@ -17,7 +17,8 @@ const AppContainer = ({ Component, pageProps }: any) => {
         <link rel="shortcut icon" href="/profile.jpg" />
         <title>Clarence Siew</title>
       </Head>
-      <BackToHome />
+      <div className="randomAnimationBar"></div>
+      <NavBar />
       <main>
         <Component {...pageProps} />
         <BackToTop />
