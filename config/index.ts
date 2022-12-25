ae8963a979227833e09c31e0642ca69fa6fee047
Clarence Siew <clarence.siew@gmail.com>
2022-12-25 14:38:44 +1100

    Fix version number and remove unintentional nav animation

--- config/index.ts
+++ config/index.ts
@@ -10,7 +10,7 @@ export default {
     text: "Clarence Siew",
     divider: "|"
   },
-  version: "6.5",
+  version: "6.5.0",
   omdb: {
     apiKey: process.env.OMDB_API_KEY,
     host: "https://omdbapi.com"
