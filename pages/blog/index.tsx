c1480f8a5e2da5822bb3afefe5bf784fe3309eb2
Clarence Siew <clarence.siew@gmail.com>
2023-01-06 21:50:50 +1100

    Switch to dynamic blog list loading

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,14 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import Head from "next/head";
 import config from "../../config";
 import { BlogPost } from "../../lib/blog";
 import retitle from "../../lib/retitle";
 import BlogNavigationView from "../../components/app/BlogNavigationView";
 import { getRemotePosts, mapDocumentDataToPosts } from "../../firebase/posts";
+import { ContentContext } from "../_app";
 
-const Blog = ({ posts }: { posts: string }) => {
+const Blog = () => {
+  const contentContext = useContext(ContentContext);
+  const isMountedRef = useRef<any>(null);
+
+  const [posts, setPosts] = useState<BlogPost[]>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isSuccess, setIsSuccess] = useState<boolean>(true);
+
+  const getPosts = async (force?: boolean) => {
+    setIsLoading(true);
+    if (!!force || !contentContext.posts.length) {
+      console.debug("Fetching blog post list from Firestore...");
+      try {
+        const remotePosts = await getRemotePosts();
+        const mappedPosts = mapDocumentDataToPosts(
+          remotePosts.docs.map((d) => ({ id: d.id, ...d.data() }))
+        ).filter((p) => p.isPublished);
+        contentContext.posts = mappedPosts;
+        setIsSuccess(true);
+      } catch (err) {
+        if (config.debugMode) console.error(err);
+        setIsSuccess(false);
+      }
+    }
+    setPosts(contentContext.posts);
+    setIsLoading(false);
+  };
+
   useEffect(() => {
     document.getElementById(config.rootElementId)?.scrollTo({ top: 0 });
+    if (!isMountedRef.current) getPosts();
+    isMountedRef.current = true;
   }, []);
   
   return (
@@ -17,19 +47,9 @@ const Blog = ({ posts }: { posts: string }) => {
         <title>{retitle("Blog")}</title>
         <meta property="og:title" content={retitle("Blog")} key="title" />
       </Head>
-      <BlogNavigationView posts={JSON.parse(posts) as BlogPost[]} />
+      <BlogNavigationView posts={posts} isLoading={isLoading} isSuccess={isSuccess} />
     </>
   );
-};
-
-export const getServerSideProps = async () => {
-  const remotePosts = await getRemotePosts();
-  const posts = mapDocumentDataToPosts(
-    remotePosts.docs.map((d) => ({ id: d.id, ...d.data() }))
-  ).filter((p) => p.isPublished);
-  return {
-    props: { posts: JSON.stringify(posts) }
-  };
 };
 
 export default Blog;
