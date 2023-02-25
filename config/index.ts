c434ffbb2476e495bd44a326a44538fff2d36665
Clarence <clarence.siew@gmail.com>
2023-02-25 15:51:58 +1100

    [6.9.1] Style update (#74)

--- config/index.ts
+++ config/index.ts
@@ -10,7 +10,7 @@ export default {
     text: "Clarence Siew",
     divider: "|"
   },
-  version: "6.9.0",
+  version: "6.9.1",
   omdb: {
     apiKey: process.env.OMDB_API_KEY,
     host: "https://omdbapi.com"
@@ -27,7 +27,7 @@ export default {
     }
   },
   features: {
-    contactForm: true
+    contactForm: false
   },
   adminEmail: process.env.ADMIN_EMAIL,
   debugMode: process.env.DEBUG_MODE
