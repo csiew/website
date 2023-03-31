5637de7627cda8e94a6d200614474ac933ce9188
Clarence Siew <clarence.siew@gmail.com>
2023-04-01 01:10:13 +1100

    Bump version number and move Whats New to bottom of page

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,32 +23,45 @@ const Home = ({ content, lastUpdated }: { content: string, lastUpdated: number }
         content={(
           <article className="app-page pageHome">
             <h2>Clarence Siew</h2>
-            {
-              Date.now() < (new Date("2023-04-15 00:00:00").getTime())
-                ? (
-                  <div className="note">
-                    <h3>What&apos;s new</h3>
-                    <p>Version {config.version}</p>
-                    <ul>
-                      <li>Refreshed design</li>
-                      <li>New post: <Link href="/posts/different-crowds">Different crowds</Link></li>
-                      <li>New pages: <Link href="/now">/now</Link>, <Link href="/feed">/feed</Link></li>
-                      <li><a href="/rss.xml" target="_blank">RSS feeds</a> and <a href="/sitemap.xml" target="_blank">Sitemap</a> generated server-side</li>
-                      <li>Site-wide search functionality (see the search button on the navbar)</li>
-                    </ul>
-                    <span style={{ color: "var(--fg-color-6)", fontStyle: "italic", fontSize: "0.8rem" }}>
-                      Last updated: {(new Date(lastUpdated)).toDateString()}
-                    </span>
-                  </div>
-                )
-                : <></>
-            }
             <ReactMarkdown linkTarget="_blank">
               {content}
             </ReactMarkdown>
             <p>
               You could also email me at <span className="no-select" style={{ fontWeight: "bold" }}>clarence.siew@gmail.com</span>.
             </p>
+            <div className="note">
+              <h3>What&apos;s new</h3>
+              <section>
+                <p style={{ fontWeight: "bold", fontStyle: "italic" }}>Version 6.11.1</p>
+                <p>
+                  <a href="https://github.com/csiew/website/pull/82" target="_blank" rel="noreferrer">
+                    See pull request for version 6.11.1
+                  </a>
+                </p>
+                <ul>
+                  <li>Fixed invalid article URLs in generated RSS feeds</li>
+                </ul>
+              </section>
+              <hr />
+              <section>
+                <p style={{ fontWeight: "bold", fontStyle: "italic" }}>Version 6.11.0</p>
+                <p>
+                  <a href="https://github.com/csiew/website/pull/81" target="_blank" rel="noreferrer">
+                    See pull request for version 6.11.0
+                  </a>
+                </p>
+                <ul>
+                  <li>Refreshed design</li>
+                  <li>New post: <Link href="/posts/different-crowds">Different crowds</Link></li>
+                  <li>New pages: <Link href="/now">/now</Link>, <Link href="/feed">/feed</Link></li>
+                  <li><a href="/rss.xml" target="_blank">RSS feeds</a> and <a href="/sitemap.xml" target="_blank">Sitemap</a> generated server-side</li>
+                  <li>Site-wide search functionality (see the search button on the navbar)</li>
+                </ul>
+              </section>
+              <span style={{ color: "var(--fg-color-6)", fontStyle: "italic", fontSize: "0.8rem" }}>
+                Last updated: {(new Date(lastUpdated)).toDateString()}
+              </span>
+            </div>
           </article>
         )}
       />
