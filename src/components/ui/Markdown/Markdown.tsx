e10731c4c210daf39bafe094ffe2d3d05ac8a069
Clarence Siew <clarence.siew@gmail.com>
2023-11-30 20:20:08 +1100

    Safer and async markdown parsing

--- src/components/ui/Markdown/Markdown.tsx
+++ src/components/ui/Markdown/Markdown.tsx
@@ -4,20 +4,37 @@ import React, { useEffect, useRef, useState } from "react";
 import { marked } from "marked";
 import HTMLReactParser from "html-react-parser";
 
-export default function Markdown({ children }: { children: string }) {
+export default function Markdown({ children }: { children?: string }) {
   const isMountedRef = useRef<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
   const [mdHtml, setMdHtml] = useState<string>("");
 
-  async function setValue() {
-    setMdHtml(await marked.parse(children));
+  async function setValue(v: string) {
+    try {
+      setMdHtml(await marked.parse(v));
+      setIsError(false);
+    } catch (err) {
+      console.error(err);
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   useEffect(() => {
-    if (!isMountedRef.current) {
+    if (!isMountedRef.current && children?.length) {
+      setIsLoading(true);
       isMountedRef.current = true;
-      setValue();
+      setValue(children);
     }
-  }, []);
+  }, [children]);
 
-  return <>{HTMLReactParser(mdHtml)}</>;
+  return (
+    <>
+      {isError && <p>Failed to parse markdown</p>}
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && !isError && HTMLReactParser(mdHtml)}
+    </>
+  );
 }
