a59277ae9547c5724dfc3ce208b6cbdea4cd3700
Clarence Siew <clarence.siew@gmail.com>
2025-02-15 13:04:35 +1100

    Upgrade packages and switch to React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
+import { optimizeLodashImports } from "@optimize-lodash/rollup-plugin";
+import ReactCompiler from "babel-plugin-react-compiler";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react({
+        babel: {
+          plugins: [ReactCompiler],
+        },
+      }),
+      optimizeLodashImports()
+    ],
   assetsInclude: ["**/*.md"],
   build: {
-    outDir: "./dist"
+    outDir: "./dist",
+    rollupOptions: {
+      output: {
+        minifyInternalExports: true
+      }
+    },
+    minify: true,
+    cssMinify: true,
+    reportCompressedSize: true,
   }
 });
