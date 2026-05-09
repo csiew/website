5655f5d07b483ac90ae392c9ff32c6e1060d418c
Clarence Siew <clarence.siew@gmail.com>
2026-05-09 23:23:58 +1000

    Upgrade packages and replacement React Icons with Lucide Icons

--- src/components/app/LinkGrid/LinkGrid.tsx
+++ src/components/app/LinkGrid/LinkGrid.tsx
@@ -1,42 +1,42 @@
 import React from "react";
-import { SiBluesky, SiGithub, SiInstagram, SiLinkedin, SiMastodon, SiThreads } from "react-icons/si";
+import { AtSign, BriefcaseBusiness, Camera, Cloud, GitBranch, MessageCircle } from "lucide-react";
 import "./LinkGrid.css";
 
 const links = [
   {
     title: "LinkedIn",
     url: "https://linkedin.com/in/clarencesiew",
-    icon: <SiLinkedin />,
+    icon: <BriefcaseBusiness />,
     newTab: true
   },
   {
     title: "GitHub",
     url: "https://github.com/csiew",
-    icon: <SiGithub />,
+    icon: <GitBranch />,
     newTab: true
   },
   {
     title: "Mastodon",
     url: "https://mastodon.online/@csiew",
-    icon: <SiMastodon />,
+    icon: <MessageCircle />,
     newTab: true
   },
   {
     title: "Bluesky",
     url: "https://bsky.app/profile/csiew.bsky.social",
-    icon: <SiBluesky />,
+    icon: <Cloud />,
     newTab: true
   },
   {
     title: "Threads",
     url: "https://threads.net/@clarence_siew",
-    icon: <SiThreads />,
+    icon: <AtSign />,
     newTab: true
   },
   {
     title: "Instagram",
     url: "https://instagram.com/clarence_siew",
-    icon: <SiInstagram />,
+    icon: <Camera />,
     newTab: true
   }
 ];
